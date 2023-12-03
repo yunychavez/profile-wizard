95f9697c3ab9f8fd69757f185dc00ee7c83b5cf9
yunychavez <yuneisy.chavezmasson@gmail.com>
2023-12-03 18:43:57 -0500

    Wizard POC

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-function showPersonalInformationPanel() {
-  document.querySelector(".personal-information").classList.remove("hidden");
-}
-
-function hideEducationalInformationPanel() {
-  document.querySelector(".educational-information").classList.add("hidden");
-}
-
-function showEducationalInformationPanel() {
-  document.querySelector(".educational-information").classList.remove("hidden");
-}
-
-function hidePersonalInformationPanel() {
-  document.querySelector(".personal-information").classList.add("hidden");
-}
-
-function showProfessionalInformationPanel() {
-  document
-    .querySelector(".professional-information")
-    .classList.remove("hidden");
-}
-
-function hideProfessionalInformationPanel() {
-  document.querySelector(".professional-information").classList.add("hidden");
-}
-
 function startWizard(event) {
   event.target.classList.add("hidden");
   showPersonalInformationPanel();
@@ -46,6 +20,73 @@ function backToStep2(event) {
   showEducationalInformationPanel();
   hideProfessionalInformationPanel();
 }
+function finishWizard(event) {
+  showProfileSumary();
+  hideProfessionalInformationPanel();
+
+  var emptyMessage = "empty";
+  document.querySelector("#firstName").innerHTML =
+    profileData["firstName"] === undefined
+      ? emptyMessage
+      : profileData["firstName"];
+
+  document.querySelector("#middleName").innerHTML =
+    profileData["middleName"] === undefined
+      ? emptyMessage
+      : profileData["middleName"];
+  document.querySelector("#lastName").innerHTML =
+    profileData["lastName"] === undefined
+      ? emptyMessage
+      : profileData["lastName"];
+  document.querySelector("#age").innerHTML =
+    profileData["age"] === undefined ? emptyMessage : profileData["age"];
+  document.querySelector("#phoneNumber").innerHTML =
+    profileData["phoneNumber"] === undefined
+      ? emptyMessage
+      : profileData["phoneNumber"];
+  document.querySelector("#email").innerHTML =
+    profileData["email"] === undefined ? emptyMessage : profileData["email"];
+  document.querySelector("#highscoolInstitution").innerHTML =
+    profileData["highscoolInstitution"] === undefined
+      ? emptyMessage
+      : profileData["highscoolInstitution"];
+  document.querySelector("#highschoolStartYear").innerHTML =
+    profileData["highschoolStartYear"] === undefined
+      ? emptyMessage
+      : profileData["highschoolStartYear"];
+  document.querySelector("#higschoolEndYear").innerHTML =
+    profileData["higschoolEndYear"] === undefined
+      ? emptyMessage
+      : profileData["higschoolEndYear"];
+  document.querySelector("#collegeInstitution").innerHTML =
+    profileData["collegeInstitution"] === undefined
+      ? emptyMessage
+      : profileData["collegeInstitution"];
+  document.querySelector("#collegeStartYear").innerHTML =
+    profileData["collegeStartYear"] === undefined
+      ? emptyMessage
+      : profileData["collegeStartYear"];
+  document.querySelector("#collegeEndYear").innerHTML =
+    profileData["collegeEndYear"] === undefined
+      ? emptyMessage
+      : profileData["collegeEndYear"];
+  document.querySelector("#companyName").innerHTML =
+    profileData["companyName"] === undefined
+      ? emptyMessage
+      : profileData["companyName"];
+  document.querySelector("#role").innerHTML =
+    profileData["role"] === undefined ? emptyMessage : profileData["role"];
+  document.querySelector("#startDate").innerHTML =
+    profileData["startDate"] === undefined
+      ? emptyMessage
+      : profileData["startDate"];
+  document.querySelector("#skills").innerHTML =
+    profileData["skills"] === undefined ? emptyMessage : profileData["skills"];
+}
+function startAgain(event) {
+  document.querySelector("#start-wizard").classList.remove("hidden");
+  hideProfileSumary();
+}
 
 document.querySelector("#start-wizard").addEventListener("click", startWizard);
 
@@ -60,6 +101,12 @@ document
   .querySelector("#back-to-step-2")
   .addEventListener("click", backToStep2);
 
+document
+  .querySelector("#finish-wizard")
+  .addEventListener("click", finishWizard);
+
+document.querySelector("#start-again").addEventListener("click", startAgain);
+
 var profileData = {};
 
 var inputs = document.querySelectorAll("input");
@@ -71,15 +118,6 @@ function setValueFromInput(event) {
   profileData[name] = value;
 }
 
-function finishWizard() {
-  document.querySelector("#firstName").innerHTML = profileData["firstName"];
-  document.querySelector("#middleName").innerHTML = profileData["middleName"];
-}
-
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("input", setValueFromInput);
 }
-
-document
-  .querySelector("#finish-wizard")
-  .addEventListener("click", finishWizard);
---
+++ nav_functions.js
@@ -0,0 +1,32 @@
+function showPersonalInformationPanel() {
+  document.querySelector(".personal-information").classList.remove("hidden");
+}
+
+function hideEducationalInformationPanel() {
+  document.querySelector(".educational-information").classList.add("hidden");
+}
+
+function showEducationalInformationPanel() {
+  document.querySelector(".educational-information").classList.remove("hidden");
+}
+
+function hidePersonalInformationPanel() {
+  document.querySelector(".personal-information").classList.add("hidden");
+}
+
+function showProfessionalInformationPanel() {
+  document
+    .querySelector(".professional-information")
+    .classList.remove("hidden");
+}
+
+function hideProfessionalInformationPanel() {
+  document.querySelector(".professional-information").classList.add("hidden");
+}
+
+function showProfileSumary() {
+  document.querySelector(".profile-sumary").classList.remove("hidden");
+}
+function hideProfileSumary() {
+  document.querySelector(".profile-sumary").classList.add("hidden");
+}
